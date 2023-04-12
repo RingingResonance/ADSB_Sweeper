b8c9f9cdc4b1c6b7416f3e410e93fb8e88af6351
perpetualwalnut <madeofsolder@gmail.com>
2023-04-11 21:35:57 -0500

    Updated startup script.

--- RADARstart.sh
+++ RADARstart.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-dump1090-mutability | /home/pi/XYaudio/software/adsbSweeper/adsbSweeper -D -a 32.831901 -o -97.088838 -R 3 -m 3 -s 300 &
-sleep 2
 sudo cpufreq-set -u 1.2GHz -d 1.2GHz
+sleep 2
+dump1090-mutability | /home/pi/XYaudio/software/adsbSweeper/adsbSweeper -D -a 32.831901 -o -97.088838 -R 3 -m 3 -s 400 -B 2 &
+sleep 2
 PID=$(pgrep --newest adsbSweeper)
 sudo renice -n -18 -p $PID
-
-sleep 1
-PID=$(pgrep --newest vlc)
-sudo renice -n 7 -p $PID
