a827f80e52a74034abad990fbc804ad50417aaee
perpetualwalnut <madeofsolder@gmail.com>
2023-06-04 18:26:17 -0500

    set startup scripts to auto-find location.

--- RADARstart.sh
+++ RADARstart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-dump1090-mutability | /home/pi/XYaudio/software/adsbSweeper/adsbSweeper -D -C -a 32.831937 -o -97.088730 -R 5 -m 5
+dump1090-mutability | /home/pi/XYaudio/software/adsbSweeper/adsbSweeper -D -C -R 5 -m 5
 PID=$(pgrep --newest adsbSweeper)
 sudo renice -n -20 -p $PID
