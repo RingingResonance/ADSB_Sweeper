3ee18698638357ae23bfa340d92d7a0845fbae33
perpetualwalnut <madeofsolder@gmail.com>
2023-06-14 22:34:44 -0500

    Fixed missing '-I' option on terminal output version of startup script.

--- RADARstartDT80.sh
+++ RADARstartDT80.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-dump1090-mutability --max-range 4 | /home/pi/XYaudio/software/adsbSweeper/adsbSweeper -w 10 -f 4000000 -D -d 530 -C -r 5000 -a 32.831937 -o -97.088730 -R 5 -m 5
+dump1090-mutability --max-range 4 | /home/pi/XYaudio/software/adsbSweeper/adsbSweeper -w 10 -f 4000000 -D -d 530 -C -I -r 5000 -R 5 -m 5
 PID=$(pgrep --newest adsbSweeper)
 sudo renice -n -20 -p $PID
